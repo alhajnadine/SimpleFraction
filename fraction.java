4dc0850ee87b110dbdc8d6fbf25ac7db240369f0
alhajnadine <62381122+alhajnadine@users.noreply.github.com>
2021-10-01 12:40:29 +0200

    fraction.java

--- fraction.java
+++ fraction.java
@@ -1,4 +1,15 @@
 public class fraction  {
     
+    int a;
+    int b;
+
+    public fraction(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+    public  void f (int a, int b){
+        System.out.println(a + "/" + b);
+       }
     
-}
+    }
--- main.java
+++ main.java
@@ -1,7 +1,10 @@
+
+
 public class main
  {
     public static void main (String[] args) {
-        System.out.println("Hello");
+        fraction ex = new fraction(1,5);
+        ex.f(1,5);
     }
 
 }
